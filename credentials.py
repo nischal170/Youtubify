23fdd20ed8529f68c9c8ce16b37d3be9dd0f56e4
NischalNeupane <nischalneupane17@gmail.com>
2020-04-15 11:44:01 +0545

    final touchups

--- credentials.py
+++ credentials.py
@@ -1,7 +1,7 @@
 #spotify
-spotify_token = "BQD2Ha-xWvopaCS3-exdlXUVeBG9wNgiA-6vFPZuZDIXKpDsZ22M3G5qH-B8ZelbX6uNzkRnZPpNdDkGHAZ6LnXtotv3kD3WUcavNDiOBn1yteB0KeXTsVdV6Q4pnU8Ygz_R-TfRpPduDjnByMp7UtxSDbrNACNH6B4k5PLTxvClYq9aho9zhNDw9hY_dKolJrRXRNQ"
-spotify_client_id = "3c09b102f81b4cc0bfbe1720d41278f1"
-user_id='xgdrv6yfxi23xr1jlthjmcch1'
+spotify_token = ""
+spotify_client_id = ""
+user_id=''
 
 #youtube
 #scopes = ["https://www.googleapis.com/auth/youtube.force-ssl"]
